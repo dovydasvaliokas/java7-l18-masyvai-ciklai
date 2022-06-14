1dfc4d7c2c58704da86256c7a9919469fcf20940
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-14 14:19:12 +0300

    Pabaigti pavyzdziai su ciklais ir masyvais

--- antras-pvz-ciklai/script.js
+++ antras-pvz-ciklai/script.js
@@ -7,6 +7,15 @@ window.onload = function() {
 
     let suma = masyvoSuma(pazymiai);
     console.log(suma);
+
+    const lyginiaiSkaiciai = issaugotiLyginiusNuo200Iki360();
+  //  isvedaMasyva(lyginiaiSkaiciai);
+    console.log(masyvoSuma(lyginiaiSkaiciai));
+
+    console.log("------------------------------");
+
+    const lyginiaiSkaiciai2 = issaugotiLyginiusNuo205Iki379();
+    isvedaMasyva(lyginiaiSkaiciai2);
 }
 
 
@@ -24,4 +33,30 @@ function masyvoSuma(masyvas) {
         suma += masyvas[i];             // +=   sumuoja (prideda prie pirminės reikšmės)
     }
     return suma;
+}
+
+
+function issaugotiLyginiusNuo200Iki360() {
+    const masyvas = [];
+    for (let i = 200; i <= 360; i += 2) {
+        masyvas.push(i);
+    }
+    return masyvas;
+}
+
+
+
+
+function issaugotiLyginiusNuo205Iki379() {
+    const masyvas = [];
+    for (let i = 205; i <= 379; i++) {
+        if (arLyginis(i)) {
+            masyvas.push(i);
+        }
+    }
+    return masyvas;
+}
+
+function arLyginis(x) {
+    return x % 2 === 0;
 }
