8fd3e497340c51f0555e98193161a6a59e9930dd
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-14 13:56:35 +0300

    Baigta masyvo sumos funkcija

--- antras-pvz-ciklai/script.js
+++ antras-pvz-ciklai/script.js
@@ -1,3 +1,27 @@
 window.onload = function() {
-    
+    const pazymiai = [8, 6, 9, 10, 10, 4, 8, 6, 7, 10];
+
+    console.log(pazymiai);
+
+    isvedaMasyva(pazymiai);
+
+    let suma = masyvoSuma(pazymiai);
+    console.log(suma);
+}
+
+
+function isvedaMasyva(masyvas) {
+    for (let i = 0; i < masyvas.length; i++) {
+        console.log(masyvas[i]);
+    }
+}
+
+
+function masyvoSuma(masyvas) {
+    let suma = 0;
+    for (let i = 0; i < masyvas.length; i++) {
+       // suma = suma + masyvas[i];   // čia yra sumavimo ilgesnis variantas
+        suma += masyvas[i];             // +=   sumuoja (prideda prie pirminės reikšmės)
+    }
+    return suma;
 }
