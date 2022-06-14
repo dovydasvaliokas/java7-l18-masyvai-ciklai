f055a72d9a276e165f1a5a98626c51d6f101923b
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-14 13:25:45 +0300

    Pabaigtas pirmas pvz

--- pirmas-pvz-masyvai/script.js
+++ pirmas-pvz-masyvai/script.js
@@ -16,4 +16,46 @@ window.onload = function() {
     atlyginimai.push(2412);
 
     console.log(atlyginimai);
+
+    atlyginimai.push(1000);
+    atlyginimai.push(1500);
+
+    console.log(atlyginimai);
+
+    // Išvedu trečią atlyginimą
+    console.log(atlyginimai[2]);
+
+    // Suskaičiuoju ir išvedu trečio ir šešto atlyginimų sumą
+    let suma = atlyginimai[2] + atlyginimai[5];
+    console.log(suma);
+
+    // Ištrinu vieną paskutinį masyvo elementą
+    atlyginimai.pop();
+
+    console.log(atlyginimai);
+
+
+    // Pakeičiu trečio algą į 3000
+    atlyginimai[2] = 3000;
+
+
+    // Išvedu trečią atlyginimą po pakeitimo
+    console.log(atlyginimai[2]);
+
+
+    // Suskaičiuoju ir išvedu trečio ir šešto atlyginimų sumą
+    let suma2 = atlyginimai[2] + atlyginimai[5];
+    console.log(suma2);
+
+
+    // Ištrinu pačią pirmą algą
+    atlyginimai.shift();
+
+    console.log(atlyginimai);
+
+
+    // Įdedu algą prieš nulinę
+    atlyginimai.unshift(1750);
+    
+    console.log(atlyginimai);
 }
