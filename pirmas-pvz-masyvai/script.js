81d3b634ff71cb701c5f7c62327133b79a0e1613
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-14 12:45:12 +0300

    Pirmo pavyzdzio du mini-pavyzdziai padaryite: uzpildyto massyvo sukurimas ir elementu pushinimas i tuscia

--- pirmas-pvz-masyvai/script.js
+++ pirmas-pvz-masyvai/script.js
@@ -1,3 +1,19 @@
 window.onload = function() {
-    
+    // Susikuriu masyvą, kuris jau turi skaičius.
+    const kainos = [3, 5, 12, 35.84, 147, 25.96, 15];
+    console.log(kainos);
+
+
+    // Susikuriu tuščią masyvą, į kurį įdėsiu elementus naudojant masyvo funkciją.
+    const atlyginimai = [];
+    atlyginimai.push(1200);
+    atlyginimai.push(1500);
+    atlyginimai.push(5242);
+    atlyginimai.push(5123);
+    atlyginimai.push(1200);
+    atlyginimai.push(1252);
+    atlyginimai.push(1265);
+    atlyginimai.push(2412);
+
+    console.log(atlyginimai);
 }
